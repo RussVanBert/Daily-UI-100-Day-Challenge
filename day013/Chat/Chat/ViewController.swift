d961c4a2ca3f5d8f73f368a597e45cd460c12597
RussVanBert <russ_vb@yahoo.com.au>
2017-06-20 22:21:54 +1000

    Day 13 completed

---
+++ day013/Chat/Chat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chat
+//
+//  Created by Russell Van Bert on 6/13/17.
+//  Copyright © 2017 Russell Van Bert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
